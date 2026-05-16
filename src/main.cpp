d35ff7c3c9d072b51fcb20afa6adc27991b37f62
lildevilzz <sugehartodanielle@gmail.com>
2026-05-16 19:27:18 +0700

    debug main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,14 @@
 
 #include "../include/Cipher.h"
 #include "../include/XorCipher.h"
-#include "../include/CaesarCipher.h"
-#include "../include/ShiftCipher.h"
+// #include "../include/CaesarCipher.h" // Comment dulu kalau belum ada .cpp-nya
+// #include "../include/ShiftCipher.h"  // Comment dulu kalau belum ada .cpp-nya
 #include "../include/FileManager.h"
 #include "../include/SecureBuffer.h"
-#include "../include/SecureKey.h"
-#include "../include/SafeBoxException.h"
+// #include "../include/SecureKey.h"
+// #include "../include/SafeBoxException.h"
 
 using namespace std;
-
 
 Cipher* pilihAlgoritma(const string& key) {
     int pilihanAlgoritma;
@@ -27,24 +26,29 @@ Cipher* pilihAlgoritma(const string& key) {
     if (pilihanAlgoritma == 1) {
         return new XorCipher(key);
     }
-    else if (pilihanAlgoritma == 2) {
-        return new CaesarCipher(key);
-    }
-    else if (pilihanAlgoritma == 3) {
-        return new ShiftCipher(key);
-    }
+    // else if (pilihanAlgoritma == 2) {
+    //     return new CaesarCipher(key);
+    // }
+    // else if (pilihanAlgoritma == 3) {
+    //     return new ShiftCipher(key);
+    // }
 
-    return nullptr;
+    cout << "Hanya bisa pilih 1 (XOR) untuk sekarang!" << endl;
+    return new XorCipher(key); // Fallback aman
 }
 
 string inputKeyDenganValidasi() {
     string key;
     cout << "Masukkan key (minimal 8 karakter): ";
     cin >> key;
- 
-    SecureKey sk;
-    sk.setKey(key); 
-    return sk.getRawKey();
+    
+    // Sementara bypass SecureKey dulu supaya gak error, tapi tetap cek panjangnya
+    while (key.length() < 8) {
+        cout << "Key terlalu pendek! Minimal 8 karakter: ";
+        cin >> key;
+    }
+    
+    return key;
 }
 
 int main() {
@@ -71,50 +75,38 @@ int main() {
             cout << "Masukkan nama file output: ";
             cin >> outputFile;
 
-             try {
+            try {
                 string key = inputKeyDenganValidasi();
- 
                 FileManager fileManager;
  
                 if (!fileManager.exists(inputFile)) {
-                    throw FileNotFoundException(inputFile);
+                    throw runtime_error("File input tidak ditemukan!");
                 }
  
                 Cipher* cipher = pilihAlgoritma(key);
-                if (cipher == nullptr) {
-                    cout << "Algoritma tidak valid!" << endl;
-                    continue;
-                }
  
+                // 1. Baca file asli
                 vector<char> data = fileManager.readBinaryFile(inputFile);
  
-                SecureBuffer<char> buffer;
-                buffer.setData(data);
+                // 2. Enkripsi datanya (Menggunakan Polymorphism)
+                vector<char> encrypted = cipher->encrypt(data);
  
-                fileManager.encryptFile(inputFile, *cipher);
+                // 3. TULIS HASILNYA KE OUTPUTFILE YANG KAMU MAU!
+                fileManager.writeBinaryFile(outputFile, encrypted);
  
-                //cb
+                // 4. Hapus file asli (sesuai permintaan kamu)
                 if (fileManager.removeFile(inputFile)) {
                     cout << "File asli berhasil dihapus." << endl;
                 } else {
                     cout << "Warning: file asli gagal dihapus." << endl;
                 }
-                //cb
+
                 delete cipher;
-
-                 cout << "File berhasil dienkripsi ke: " << outputFile << endl;
-
-            }
-            catch (const invalid_argument& e) {
-                cout << "Key tidak valid: " << e.what() << endl;
-            }
-            catch (const SafeBoxException& e) {
-                cout << e.what() << endl;
+                cout << "File berhasil dienkripsi ke: " << outputFile << endl;
             }
             catch (const exception& e) {
                 cout << "Error: " << e.what() << endl;
             }
-
         }
         else if (pilihan == 2) {
             string inputFile, outputFile;
@@ -127,48 +119,32 @@ int main() {
 
             try {
                 string key = inputKeyDenganValidasi();
- 
                 FileManager fileManager;
  
                 if (!fileManager.exists(inputFile)) {
-                    throw FileNotFoundException(inputFile);
+                    throw runtime_error("File input tidak ditemukan!");
                 }
  
                 Cipher* cipher = pilihAlgoritma(key);
-                if (cipher == nullptr) {
-                    cout << "Algoritma tidak valid!" << endl;
-                    continue;
-                }
  
+                // 1. Baca file terenkripsi
                 vector<char> data = fileManager.readBinaryFile(inputFile);
  
-                SecureBuffer<char> buffer;
-                buffer.setData(data);
- 
-                vector<char> decrypted = cipher->decrypt(buffer.getData());
+                // 2. Dekripsi datanya (Menggunakan Polymorphism)
+                vector<char> decrypted = cipher->decrypt(data);
 
+                // 3. TULIS HASILNYA KE OUTPUTFILE!
                 fileManager.writeBinaryFile(outputFile, decrypted);
  
-                //cb
-                fileManager.decryptFile(inputFile, *cipher);
-
+                // 4. Hapus file terenkripsi
                 if (fileManager.removeFile(inputFile)) {
                     cout << "File terenkripsi berhasil dihapus." << endl;
                 } else {
                     cout << "Warning: file terenkripsi gagal dihapus." << endl;
                 }
-                //cb
-                delete cipher;
 
+                delete cipher;
                 cout << "File berhasil didekripsi ke: " << outputFile << endl;
-
-                delete cipher;
-            }
-            catch (const invalid_argument& e) {
-                cout << "Key tidak valid: " << e.what() << endl;
-            }
-            catch (const SafeBoxException& e) {
-                cout << e.what() << endl;
             }
             catch (const exception& e) {
                 cout << "Error: " << e.what() << endl;
